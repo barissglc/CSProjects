c2d683d910e51042f08125378765a90674565fc0
Barış Güleç <87657907+barissglc@users.noreply.github.com>
2023-10-16 10:47:20 +0300

    Add files via upload

---
+++ 4ISLEM/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = (sayi1 + sayi2).ToString();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = (sayi1 - sayi2).ToString();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = (sayi1 / sayi2).ToString();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = (sayi1 * sayi2).ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+    }
+}
